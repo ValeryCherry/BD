39020a7fcf74aab0189b3cdfa070ab387a51ac5f
Valery <alkid007@yandex.ru>
2022-06-27 13:45:11 +0300

    new file commit

---
+++ DZ_OOP.py
@@ -0,0 +1,19 @@
+class TheMetod:
+        def metod1(self, string):
+            self.glas = 'aouyei'
+            self.overall = len(string)
+            self.vow = ''
+            self.cons = ''
+            for i in metod1:
+                if i in self.glas:
+                    self.vow += i
+                else:
+                    self.cons += i
+                if len(self.vow) * len(self.cons) <= self.overall:
+                    return self.vow
+            return self.cons
+
+
+        def metod2 (self):
+            return len(str())
+
